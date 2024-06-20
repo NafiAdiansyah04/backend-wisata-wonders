578acfbc6ab8ca57392f30522ccb2bb70d3921f8
NafiAdiansyah04 <nafiadiansyah04@gmail.com>
2024-06-20 17:11:57 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,6 @@
 const Hapi = require('@hapi/hapi');
-const connectToDatabase = require('./db'); // Ganti dengan lokasi file koneksi Anda
-const ArticleRoutes = require('./routes/articleRoutes');
-const EventRoutes = require('./routes/eventRoutes');
 
 const init = async () => {
-  await connectToDatabase(); // Memanggil fungsi koneksi database
-
   const server = Hapi.server({
     port: process.env.PORT || 3030,
     host: '0.0.0.0',
@@ -16,16 +11,17 @@ const init = async () => {
     },
   });
 
-  server.route(ArticleRoutes);
-  server.route(EventRoutes);
+  // Menangani permintaan favicon dengan respons kosong
+  server.route({
+    method: 'GET',
+    path: '/favicon.ico',
+    handler: (request, h) => {
+      return h.response('').code(204);
+    }
+  });
 
-  try {
-    await server.start();
-    console.log(`Server running at ${server.info.uri}`);
-  } catch (error) {
-    console.error('Error starting server:', error);
-    process.exit(1); // Exit process with error
-  }
+  await server.start();
+  console.log(`Server running in ${server.info.uri}`);
 };
 
 init();
