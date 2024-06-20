1b742db899793a5f86be14dc06b165c806f4715f
NafiAdiansyah04 <nafiadiansyah04@gmail.com>
2024-06-20 13:50:54 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ const Hapi = require('@hapi/hapi');
 const mongoose = require('mongoose');
 const ArticleRoutes = require('./routes/articleRoutes');
 const EventRoutes = require('./routes/eventRoutes');
- 
+
 const init = async () => {
   const server = Hapi.server({
-    port: 3030,
-    host: 'localhost',
+    port: process.env.PORT || 3030,
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -19,16 +19,16 @@ const init = async () => {
   });
 
   const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('Connection with database succeeded.');
+  db.on('error', console.error.bind(console, 'connection error'));
+  db.once('open', () => {
+    console.log('Connection with database succeeded.');
   });
 
   server.route(ArticleRoutes);
   server.route(EventRoutes);
- 
+
   await server.start();
   console.log(`Server running in ${server.info.uri}`);
 };
- 
+
 init();
